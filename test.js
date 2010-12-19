0ac254b556c42a76ec08c237b367eca0d47f91b1
Bill Casarin <jb@jb55.com>
2010-12-19 10:22:22 -0500

    Changed test slightly

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-var bitcoin = require('./lib/bitcoin/index.js');
+var bitcoin = require('./lib/bitcoin');
 var client = new bitcoin.Client('localhost', 8332, 'jb55', 'thisisthepassword');
 
 doCmd('getBalance');
@@ -8,10 +8,13 @@ doCmd('getDifficulty');
 doCmd('getInfo');
 doCmd('getHashesPerSecond');
 doCmd('getGenerate');
+doCmd('listTransactions');
 
 function doCmd(cmd) {
   client[cmd](function(data) {
     console.log(cmd);
     console.log(data);
+    console.log('');
   });
 }
+
