938cdfd50067ac2b227eb117397a3766a44223a8
Bill Casarin <bill@casarin.ca>
2011-05-17 09:03:11 -0400

    Breaking changes:
    * All json callbacks now receive an err as its first argument
    * Added getAccount

--- test/api.js
+++ test/api.js
@@ -1,70 +1,70 @@
 var path = require('path');
 require.paths.unshift(path.join(__dirname, '..'));
 
+// test variables
+
+var test = {
+  address: "1GbDSt8XieCHQmkQsZmMirwHoUHJukjwXn"
+}
+
+// end test variables
+
 var vows = require('vows'),
     assert = require('assert');
 
 var bitcoin = require('lib/bitcoin');
 
-var nullError = function(cb) { 
-  var self = this;
-  return function() { 
-    var args = [].slice.call(arguments);
-    args.unshift(null);
-    cb.apply(self, args);
-  };
-}
-
 function makeClient() {
   return new bitcoin.Client('localhost', 8332, 'jb55', 'thisisthepassword');
 }
 
-function clientTopic() {
-  var args = [].slice.call(arguments);
-  var cmd = args.shift();
-  return function(client){
-    args.push(nullError(this.callback));
-    client[cmd].apply(client, args);
-  }
-}
-
-function notEmpty(err, data) {
+function notEmpty(data) {
   if (data === 0)
     return;
   assert.ok(data);
 }
 
 vows.describe('api').addBatch({
-  'client': {
-    topic: makeClient(),
-    'getTransaction': {
-      topic: clientTopic('getTransaction', "fff2525b8931402dd09222c50775608f75787bd2b87e56995a7bdd30f79702c4"),
-      'should not be empty': notEmpty,
+  '': {
+    topic: makeClient,
+    'an account': {
+      topic: function(client){
+        client.getAccount(test.address, this.callback);
+      },
+      'is valid': function(account){
+        assert.ok(account, "Update test variables with a valid address?");
+      },
     },
     'getDifficulty': {
-      topic: clientTopic('getDifficulty'),
+      topic: function(client) { client.getDifficulty(this.callback); },
       'should not be empty': notEmpty,
-      'is a number': function (err, data) { assert.isNumber(data) },
-      'is greater than 0': function (err, data) { assert.isTrue(data > 0); }
+      'is a number': function (data) {
+        assert.isNumber(data);
+      },
+      'is greater than 0': function (data) { assert.isTrue(data > 0); }
     },
     'getInfo': {
-      topic: clientTopic('getInfo'),
+      topic: function(client) { client.getInfo(this.callback); },
       'should not be empty': notEmpty,
-      'info.errors should be empty': function (err, info) {
+      'info.errors should be empty': function (info) {
         assert.isEmpty(info.errors);
       },
     },
     'getHashesPerSec': {
-      topic: clientTopic('getHashesPerSec'),
+      topic: function(client) { client.getHashesPerSec(this.callback); },
       'should not be empty': notEmpty,
-      'is a number': function (err, data) { assert.isNumber(data) },
+      'is a number': function (data) { assert.isNumber(data) },
     },
     'help': {
-      topic: clientTopic('help'),
+      topic: function(client) { client.help(this.callback); },
       'should not be empty': notEmpty,
-    }
+    },
+    'getTransaction': {
+      topic: "TODO: get valid transaction",
+      'should not be empty': notEmpty,
+    },
   },
-  
+
 }).run();
 
 
