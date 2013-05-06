8f1a886dc7e0af2fdbf1032cde6cc690bc1a94a3
freewil <sean@eternalrise.com>
2013-05-06 01:46:43 -0400

    check commands against `help` more thoroughly

--- test/missing-commands.js
+++ test/missing-commands.js
@@ -3,34 +3,75 @@ var assert = require('assert'),
     config = require('./config'),
     commands = require('../lib/commands');
     
+    
+var getHelpCommands = function(client, cb) {
+  var commandRegex = /^([a-z]+)/;
+  client.cmd('help', function(err, commandList) {
+    if (err) return cb(err);
+    
+    var helpCommands = [];
+    
+    // split up the command list by newlines
+    var commandListLines = commandList.split('\n');
+    commandListLines.forEach(function(commandListLine) {
+      
+      // get the actual name of the command from the command list line
+      var result = commandRegex.exec(commandListLine);
+      if (!result) {
+        return cb(new Error('command list line failed to match regex: ' + commandListLine));
+      }
+      helpCommands.push(result[1]);
+    });
+    
+    cb(null, helpCommands);
+  });
+};
+    
 describe('Client Commands', function() {
   
   it('should have all the commands listed by `help`', function(done) {
     var client = new bitcoin.Client(config);
-    var commandRegex = /^([a-z]+)/;
-    
-    // get the list of all the commands
-    client.cmd('help', function(err, commandList) {
+    getHelpCommands(client, function(err, helpCommands) {
       assert.ifError(err);
       
-      // split up the command list by newlines
-      var commandListLines = commandList.split('\n');
-      commandListLines.forEach(function(commandListLine) {
-        
-        // get the actual name of the command from the command list line
-        var result = commandRegex.exec(commandListLine);
-        assert.ok(result, 'command list line failed to match regex: ' + commandListLine);
-        
-        // check to see if the command name is in the commands object 
-        var found = false;
-        for (var i in commands) {
-          if (commands[i] === result[1]) {
+      for (var i in helpCommands) {
+        var found = true;
+        for (var j in commands) {
+          if (commands[j] === helpCommands[i]) {
             found = true;
             break;
           }
         }
-        assert.ok(found, 'missing command found in `help`: ' + result[1]);
-      });
+        assert.ok(found, 'missing command found in `help`: ' + helpCommands[i]);
+      }
+      
+      done();
+    });
+  });
+  
+  it('should not have any commands not listed by `help`', function(done) {
+    var client = new bitcoin.Client(config);
+    getHelpCommands(client, function(err, helpCommands) {
+      assert.ifError(err);
+      
+      for (var i in commands) {
+        var found = false;
+        for (var j in helpCommands) {
+          if (commands[i] === helpCommands[j]) {
+            found = true;
+            break;
+          }
+        }
+        
+        // ignore commands not found in help because they are hidden
+        // if the wallet isn't encrypted
+        var ignore = ['walletlock', 'walletpassphrase', 'walletpassphrasechange'];
+        if (~ignore.indexOf(commands[i])) {
+          assert.ok(!found, 'command found in `help`: ' + commands[i]);
+        } else {
+          assert.ok(found, 'command not found in `help`: ' + commands[i]);
+        }
+      }
       
       done();
     });
